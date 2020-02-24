aa981390af465cbc2a4210450cadbac3d4245660
Matthew Calimbas <45830182+MattyCalimbas@users.noreply.github.com>
2020-02-24 14:00:52 -0800

    first commit

---
+++ js/demo.js
@@ -0,0 +1,14 @@
+
+// This can be used to set the Particles Effects. Check README for more details!
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    // dotColor: '#5cbdaa',
+    // lineColor: '#5cbdaa'
+    dotColor: '#3675fe',
+    lineColor: '#3675fe'
+  });
+  var intro = document.getElementById('intro');
+  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
+}, false);
+
+
